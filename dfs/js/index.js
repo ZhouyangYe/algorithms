f59e60ed41129660a56722f29b55601a4c46db96
Zhouyang Ye <zye0821@gmail.com>
2020-12-13 00:57:32 +0800

    feat: js dfs

--- dfs/js/index.js
+++ dfs/js/index.js
@@ -1,10 +1,10 @@
 const data = require('./data');
 
-const bfs = (tree, cb) => {
+const dfs = (tree, cb) => {
   const queue = [tree];
 
   while (queue.length) {
-    const node = queue.shift();
+    const node = queue.pop();
 
     if (cb(node)) {
       return node;
@@ -16,7 +16,7 @@ const bfs = (tree, cb) => {
   }
 };
 
-bfs(data, (node) => {
+dfs(data, (node) => {
   console.log(node.id);
   return false;
 });
